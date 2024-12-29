b90ce44b1e49fa3954dde3a273ab2cc0503fda94
Paul P <97414219+anthonyp16@users.noreply.github.com>
2024-12-28 21:13:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,7 @@ df = load_data(st.secrets['public_gsheets_url'])[0]
 
 df.columns = df.columns.str.replace('Over', 'O')
 df.columns = df.columns.str.replace('Under', 'U')
+df['Start'] = pd.to_datetime(df['Start'])
 df['Start'] = df['Start'].dt.strftime('%I %p')
 sportsbooks =  df.columns[df.columns.get_loc('Best Odds')+1:-1].to_list()
 
